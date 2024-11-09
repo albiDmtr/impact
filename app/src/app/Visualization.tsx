9a317a7c2a615b75f92e88edea1d5954e5a1cec9
Albert Domotor <hello@albertdomotor.ninja>
2024-11-09 18:53:44 +0200

    added earth geojson

--- app/src/app/Visualization.tsx
+++ app/src/app/Visualization.tsx
@@ -1,59 +1,54 @@
 'use client';
 import DeckGL from '@deck.gl/react';
-import StaticMap from 'react-map-gl';
-import React, {useState, useCallback, useEffect} from 'react';
-import { LinearInterpolator } from '@deck.gl/core';
-import {HeatmapLayer} from '@deck.gl/aggregation-layers';
+import React, {useEffect} from 'react';
+import {
+  _GlobeView as GlobeView,
+  COORDINATE_SYSTEM
+} from '@deck.gl/core';
+import {ScatterplotLayer} from '@deck.gl/layers';
+import {GeoJsonLayer} from '@deck.gl/layers';
+import {SimpleMeshLayer} from '@deck.gl/mesh-layers';
+import {SphereGeometry} from '@luma.gl/engine';
+import viewStates from './viewStates.js';
 
-const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-
-const INITIAL_VIEW_STATE = {
-  latitude: 40.732777509541414,
-  longitude: -73.99210821089324,
-  zoom: 11,
-  pitch: 30,
-  bearing: 0
-};
-
-type DataType = {
-  from: [longitude: number, latitude: number];
-  to: [longitude: number, latitude: number];
-};
-
-const transitionInterpolator = new LinearInterpolator();
+const radius = 2;
 
 const Visualization = () => {
 
   // get data
-  const DATA_URL = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json';
+  const DATA_URL = 'https://raw.githubusercontent.com/albiDmtr/impact/refs/heads/main/data/testForRes.json';
   type DataPoint = [longitude: number, latitude: number, count: number];
   const layers = [
-    new HeatmapLayer<DataPoint>({
+    new SimpleMeshLayer({
+      id: 'earth-sphere',
+      data: [0],
+      mesh: new SphereGeometry({radius: 6.3e6, nlat: 36, nlong: 72}),
+      coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
+      getPosition: [0, 0, 0],
+      getColor: [34, 34, 34]
+    }),
+    new GeoJsonLayer({
+      id: 'earth-land',
+      data: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_land.geojson',
+      // Styles
+      stroked: false,
+      filled: true,
+      opacity: 0.1,
+      getFillColor: [120, 120, 120]
+    }),
+    new ScatterplotLayer<DataPoint>({
+      id: 'scatter-plot',
       data: DATA_URL,
-      id: 'heatmap-layer',
-      pickable: false,
-      getPosition: d => [d[0], d[1]],
-      getWeight: d => d[2],
-      radiusPixels: 5,
-      intensity: 2.2,
-      threshold: 0.12,
-      colorRange: [[34, 245, 80],[34, 245, 80]]
-
+      radiusScale: radius,
+      radiusMinPixels: 0.25,
+      getPosition: d => [d[0], d[1], 0],
+      getFillColor: d => (d[2] === 1 ? [255, 0, 0] : [0, 0, 255]),
+      getRadius: 1,
+      updateTriggers: {
+        getFillColor: [[255, 0, 0], [0, 0, 255], [0, 255, 0]]
+      }
     })
   ];
-
-  const [viewState, updateViewState] = useState<Record<string, any>>(INITIAL_VIEW_STATE);
-
-
-  const rotateCamera = useCallback(() => {
-    updateViewState(v => ({
-      ...v,
-      bearing: v.bearing + 30,
-      transitionDuration: 30000,
-      transitionInterpolator,
-      onTransitionEnd: rotateCamera
-    }));
-  }, []);
 
     useEffect(() => {
         document.querySelector('.mapboxgl-canvas')?.setAttribute('style', 'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; pointer-events: none;');
@@ -61,18 +56,11 @@ const Visualization = () => {
 
   return (
     <DeckGL
-    viewState={viewState}
-    controller={true}
-    onLoad={rotateCamera}
-    onViewStateChange={v => updateViewState(v.viewState)}
-    layers={layers}
+      views={new GlobeView()}
+      initialViewState={viewStates['br']}
+      controller={true}
+      layers={layers}
     >
-    <StaticMap
-      mapStyle="mapbox://styles/albidmtr/cm39gbxix01iy01pihhx11m64"
-      mapboxAccessToken={mapboxToken}
-      dragRotate={true}
-      dragPan={false}
-      doubleClickZoom={false}/>
   </DeckGL>
   );
 }
---
+++ app/src/app/viewStates.js
@@ -0,0 +1,9 @@
+const viewStates = {
+    br: {
+        latitude: -10.727900853154123,
+        longitude: -52.48799815779519,
+        zoom: 2
+    }
+}
+
+export default viewStates;
